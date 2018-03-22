2340fbc79d02e7b17a008c65aa7f9c60d53577e5
Jesse <10161205+jfrench206@users.noreply.github.com>
2018-03-22 16:41:32 -0500

    Actually fixed paths

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,27 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const PUBLIC_PATH_PREFIX = './'
+
 module.exports = {
   entry: {
     app: './src/index.js'
   },
   devtool: 'inline-source-map',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(['docs']),
     new CopyWebpackPlugin([
       { from: 'assets', to: 'assets' }
     ]),
     new HtmlWebpackPlugin({
       title: 'Output Management',
       template: 'src/index.html',
+      pathPrefix: PUBLIC_PATH_PREFIX
     })
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, ''),
-    publicPath: '/'
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: PUBLIC_PATH_PREFIX
   }
 }
