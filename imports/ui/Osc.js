3e831a2cc4679d8ae8572056d6d577e5e1b54f59
Jesse <10161205+jfrench206@users.noreply.github.com>
2018-06-05 15:17:12 -0500

    Customized Osc values

--- imports/ui/Osc.js
+++ imports/ui/Osc.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Osc extends React.Component {
 	render() {
 		return (
-			<div className="audio-module" id={this.props.id}><span>Osc 1</span><input type="checkbox" name="1" id="check1"></input></div>
+			<div className="audio-module"><span>Osc {this.props.num}</span><input type="checkbox" name={this.props.num} id={`check${this.props.num}`}></input></div>
 		);
 	}
 }
