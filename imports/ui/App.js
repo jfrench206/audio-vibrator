b0acf41109ccf2c9d632fd72923c6f107389e2b8
Jesse <10161205+jfrench206@users.noreply.github.com>
2018-06-09 15:21:20 -0500

    Event handlers moved to App component

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Osc from './Osc';
-import * as audio from './../api/audio';
+import * as Keys from './../api/keys';
 
 function Oscs(props){
 	return (
-		<div className="audio-wrapper">
+			<div className="audio-wrapper">
 			<Osc num="1"/>
 			<Osc num="2"/>
 			<Osc num="3"/>
@@ -14,11 +14,17 @@ function Oscs(props){
 }
 
 export default class App extends React.Component {
+
+	componentDidMount() {
+		window.addEventListener("keydown", Keys.keyDown, false);
+		window.addEventListener("keyup", Keys.keyUp, false);
+	}
+
 	render(){
 		return(
-			<div className="flex-container">
+			<div className="flex-container" onKeyDown={Keys.keyDown} onKeyUp={Keys.keyUp}>
 				<header><h1>Audio Vibrator</h1></header>
-				<Oscs/>
+				<Oscs />
 				<footer><h3>created by Jesse French</h3></footer>
 			</div>
 		);
--- imports/ui/Osc.js
+++ imports/ui/Osc.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import * as audio from './../api/audio';
+import oscs, * as Audio from './../api/audio';
 import * as keys from './../api/keys';
 
 export default class Osc extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {freq: audio.pitchRef};
+    this.state = {freq: Audio.pitchRef};
+  }
+
+  componentDidMount() {
+  	this.setState({
+  		freq: oscs[this.props.num-1].frequency.value.toFixed(2),
+  		setting: false
+  	});
   }
 
 	render() {
 		return (
 			<div className="audio-module">
 				<span>Osc {this.props.num}</span>
-				<input type="checkbox" name={this.props.num} id={`check${this.props.num}`}>
+				<input type="checkbox" name={this.props.num} id={`check${this.props.num}`} checked={!!this.state.setting}>
 				</input>
 				<p>{this.state.freq} Hz</p>
 			</div>
